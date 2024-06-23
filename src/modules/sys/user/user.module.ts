58b2e92d51433e553d54b37680c7ddbe7f911d95
ru <ru@qq.com>
2024-06-23 20:43:45 +0800

    user

--- src/modules/sys/user/user.module.ts
+++ src/modules/sys/user/user.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
-
+import {TypeOrmModule} from "@nestjs/typeorm";
 @Module({
+	imports:[
+		TypeOrmModule.forFeature([])
+	],
   controllers: [UserController],
   providers: [UserService],
+  
 })
 export class UserModule {}
