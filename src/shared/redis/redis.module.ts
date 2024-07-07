d4cb8f3931d051b532ca2939d48c14c375dd99f4
ru <ru@qq.com>
2024-07-07 20:52:53 +0800

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/modules/sys/user/user.controller.ts

--- src/shared/redis/redis.module.ts
+++ src/shared/redis/redis.module.ts
@@ -1,7 +1,26 @@
 import { Module } from '@nestjs/common';
 import { CacheService } from './cache.service';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { RedisOptions } from 'ioredis';
+import { CacheModule } from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-ioredis-yet'
 @Module({
+  imports: [
+    CacheModule.registerAsync(({
+      imports:[ConfigModule],
+      inject:[ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const redisOptions: RedisOptions = configService.get('redis')
+
+        return {
+          isGlobal: true,
+          store: redisStore,
+          isCacheableValue: () => true,
+          ...redisOptions,
+        }
+      },
+    }))
+  ],
   providers: [CacheService],
 })
 export class RedisModule {}
